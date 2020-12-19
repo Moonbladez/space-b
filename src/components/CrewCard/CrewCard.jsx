c9cedbd1365a8b5d09a27b70ea2bb4392cdbf42c
Bekki <moonbladez@hotmail.com>
2020-12-19 15:40:00 +0100

    amended tabindex to 0

--- src/components/CrewCard/CrewCard.jsx
+++ src/components/CrewCard/CrewCard.jsx
@@ -8,55 +8,59 @@ import styles from "./CrewCard.module.scss";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 export default function CrewCard({ image, link, name, agency, launches }) {
-	const [isOpen, setIsOn] = useState(false);
+  const [isOpen, setIsOn] = useState(false);
 
-	const handleToggle = () => {
-		setIsOn(!isOpen);
-	};
+  const handleToggle = () => {
+    setIsOn(!isOpen);
+  };
 
-	return (
-		<div className={styles.CrewCard}>
-			<div className={styles.outer}>
-				<LazyLoadImage src={image} effect='blur' alt={name} />
-				<div
-					onClick={handleToggle}
-					className={isOpen ? styles.wrapperExpanded : styles.wrapper}
-				>
-					{!isOpen ? (
-						<div className={styles.name}>
-							{name}
-							<span tabIndex='1' onKeyPress={handleToggle}>
-								<FiChevronsUp />
-							</span>
-						</div>
-					) : (
-						<div className={styles.description}>
-							<span>
-								<FiChevronsDown tabIndex='1' onKeyPress={handleToggle} />
-							</span>
-							<ul>
-								<li>
-									Name:
-									<span>{name}</span>
-								</li>
-								{launches.length > 0 && (
-									<li>
-										Launches:
-										<span>{launches.length}</span>
-									</li>
-								)}
-								<li>
-									Agency:
-									<span>{agency}</span>
-								</li>
-								<li>
-									<a href={link}>Wikipedia </a>
-								</li>
-							</ul>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.CrewCard}>
+      <div className={styles.outer}>
+        <LazyLoadImage src={image} effect="blur" alt={name} />
+        <div
+          onClick={handleToggle}
+          className={isOpen ? styles.wrapperExpanded : styles.wrapper}
+        >
+          {!isOpen ? (
+            <div className={styles.name}>
+              {name}
+              <span tabIndex="0" onKeyPress={handleToggle}>
+                <FiChevronsUp aria-label="toggle information up" />
+              </span>
+            </div>
+          ) : (
+            <div className={styles.description}>
+              <span>
+                <FiChevronsDown
+                  tabIndex="0"
+                  onKeyPress={handleToggle}
+                  aria-label="toggle information down"
+                />
+              </span>
+              <ul>
+                <li>
+                  Name:
+                  <span>{name}</span>
+                </li>
+                {launches.length > 0 && (
+                  <li>
+                    Launches:
+                    <span>{launches.length}</span>
+                  </li>
+                )}
+                <li>
+                  Agency:
+                  <span>{agency}</span>
+                </li>
+                <li>
+                  <a href={link}>Wikipedia </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,39 +6,39 @@ import { RiTwitterLine, RiLinkedinBoxLine } from "react-icons/ri";
 import styles from "./Footer.module.scss";
 
 export function Footer() {
-	return (
-		<footer className={styles.footer}>
-			<div>
-				<nav>
-					<ul>
-						<li>
-							<a
-								href='https://github.com/Moonbladez'
-								aria-label='link to github'
-							>
-								<FaGithubAlt />
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://twitter.com/jegheterbekki'
-								aria-label='link to twitter'
-							>
-								<RiTwitterLine />
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://www.linkedin.com/in/rebecca-young83/'
-								aria-label='link to Linkedin'
-							>
-								<RiLinkedinBoxLine />
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			<div className={styles.footer__copyright}>made with love Bulu Design</div>
-		</footer>
-	);
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/Moonbladez"
+                aria-label="link to github"
+              >
+                <FaGithubAlt />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/jegheterbekki"
+                aria-label="link to twitter"
+              >
+                <RiTwitterLine />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/rebecca-young83/"
+                aria-label="link to Linkedin"
+              >
+                <RiLinkedinBoxLine />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={styles.footer__copyright}>made with love Bulu Design</div>
+    </footer>
+  );
 }
