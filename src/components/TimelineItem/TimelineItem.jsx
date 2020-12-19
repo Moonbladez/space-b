a3343db64d9f3e09e949827e6c4bc36725bbf334
Bekki <moonbladez@hotmail.com>
2020-12-19 16:12:56 +0100

    fixed contrast errors in timeline card

--- src/components/TimelineItem/TimelineItem.jsx
+++ src/components/TimelineItem/TimelineItem.jsx
@@ -1,34 +1,43 @@
 //components
-import {VscLinkExternal} from "react-icons/vsc"
+import { VscLinkExternal } from "react-icons/vsc";
 
 //styling
 import styles from "./TimelineItem.module.scss";
 
 const formateDate = (date) => {
-	let unixDate = date;
-	const milliseconds = unixDate * 1000;
-	const dateObject = new Date(milliseconds);
+  let unixDate = date;
+  const milliseconds = unixDate * 1000;
+  const dateObject = new Date(milliseconds);
 
-	const formatedDate = dateObject.toLocaleDateString("en-UK");
-	return formatedDate;
+  const formatedDate = dateObject.toLocaleDateString("en-UK");
+  return formatedDate;
 };
 
-export default function TimelineItem({timelineData}) {
-	const link = timelineData.links.article
-	//  const dateFormat = formateDate(date);
-	return (
-		<article className={styles.TimelineItem}>
-			<div className={styles.content}>
-				<h2>{timelineData.title}</h2>
-				<div className={styles.date}>{timelineData.dateFormat}</div>
-				<p>{timelineData.details}</p>
-				{link && 
-					<a href={link} target='_blank' rel="noreferrer noopener" aria-label="This is an external link (opens in a new tab)" className={styles.link}>   
-					Read more <span><VscLinkExternal/></span>
-				</a> 
-				}
-				<span className={styles.circle}></span>
-			</div>
-		</article>
-	)
+export default function TimelineItem({ timelineData }) {
+  const link = timelineData.links.article;
+  //  const dateFormat = formateDate(date);
+  return (
+    <article className={styles.TimelineItem}>
+      <div className={styles.content}>
+        <h2>{timelineData.title}</h2>
+        <div className={styles.date}>{timelineData.dateFormat}</div>
+        <p>{timelineData.details}</p>
+        {link && (
+          <a
+            href={link}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="This is an external link (opens in a new tab)"
+            className={styles.link}
+          >
+            Read more Information
+            <span>
+              <VscLinkExternal />
+            </span>
+          </a>
+        )}
+        <span className={styles.circle}></span>
+      </div>
+    </article>
+  );
 }
